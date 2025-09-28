df60f9f5cb1b8d41fbfdbbb07aa1acb0b7aca2d9
kcimino <102926538+kcimino@users.noreply.github.com>
2025-09-28 11:20:42 -0400

    fix performance issues

--- doom/+misc.el
+++ doom/+misc.el
@@ -7,7 +7,8 @@
 
 (setq org-refile-allow-creating-parent-nodes t)
 )
-
+(after! projectile
+  (add-to-list 'projectile-ignored-projects "~/org"))
 ;; Treat camelCase as two words
 (global-subword-mode 1)
 
@@ -16,5 +17,20 @@
       auto-save-default t                         ; Nobody likes to loose work, I certainly don't
       ;;truncate-string-ellipsis "…"                ; Unicode ellispis are nicer than "...", and also save /precious/ space
       password-cache-expiry nil                   ; I can trust my computers ... can't I?
-
 )
+;;(setq which-key-use-C-h-commands 't)
+
+
+
+;; from https://orgmode.org/worg/org-hacks.html#orgf722eb0
+;; This sets it to ask for a note when you clock out, could be useful to note where I left off
+(defun rgr/check-for-clock-out-note()
+      (interactive)
+      (save-excursion
+        (org-back-to-heading)
+        (let ((tags (org-get-tags)))
+          (and tags (message "tags: %s " tags)
+               (when (member "clocknote" tags)
+                 (org-add-note))))))
+
+(add-hook 'org-clock-out-hook 'rgr/check-for-clock-out-note)
