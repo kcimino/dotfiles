f2cb45fa2506a3fbdf0745af33e128ec1c9a071c
kcimino <102926538+kcimino@users.noreply.github.com>
2025-09-28 11:19:29 -0400

    add rss feed and something else

--- doom/custom.el
+++ doom/custom.el
@@ -3,9 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://rss.app/feed/9nHgYRvdVKBJskTp" "https://ms-demeanor.tumblr.com/rss"))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(org-download cmake-mode zone-nyan quarto-mode org-transform-tree-table nyan-mode docker-tramp compat auto-virtualenv async)))
+   '(org-download org-sticky-header highlight-indent-guides cmake-mode))
+ '(safe-local-variable-values
+   '((checkdoc-package-keywords-flag)
+     (projectile-ignored-projects . ~/org)
+     (org-tags-column . -105)
+     (org-refile-targets
+      ("~/org/Cooking.org" :level . 2)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
